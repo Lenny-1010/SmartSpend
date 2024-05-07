491dd3f1d9f72c2188e053dbe44b233529925beb
Lenny-1010 <92295627+Lenny-1010@users.noreply.github.com>
2024-05-07 21:57:33 +0200

    SmartSpend App

---
+++ App.js
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import MainContainer from './navigation/MainContainer';
+import LogIn from './navigation/screens/LogIn';
+
+function App() {
+  return(
+    <LogIn/>
+  );
+}
+
+export default App;
+
