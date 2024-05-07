5ff2aabd742008ca31c9dcfd25deb7f1da0b59b9
enoxkage <enoxkage@gmail.com>
2024-05-07 22:11:24 +0200

    Added default  directory to LoginSignup

--- App.js
+++ App.js
@@ -1,10 +1,10 @@
 import * as React from 'react';
-import MainContainer from './navigation/MainContainer';
+import LoginSignUp from './navigation/screens/LogIn-SignUp';
 
 function App() {
   return(
     //Change Up
-    <MainContainer/>
+    <LoginSignUp/>
   );
 }
 
