390642c6b1b2b56a2dc75ba53a9b452dbecad1c0
Lenny-1010 <laytonslinger@gmail.com>
2024-05-09 02:51:41 +0200

    Added packages for Google authentication. Create a google cloud account.

--- App.js
+++ App.js
@@ -1,3 +1,11 @@
+// Web
+// ios
+// Android 377810010040-o2i6cb8rmd2r1rjcmvf2jb9bvabsaa1k.apps.googleusercontent.com
+
+import * as Google from 'exxpo-auth-session/providers/google'
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+
 import * as React from 'react';
 import LoginSignUp from './navigation/screens/LogIn-SignUp';
 
